69bbd68ac26b7d8715163c0c055dd585e8ccdeaf
0ldStar <brosto_man@mail.ru>
2022-03-10 00:57:37 +0700

    add reverse germanium

--- 2.2.py
+++ 2.2.py
@@ -1,4 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.plot(U2, I2, label='Reverse Germanium', c='r')
+I4 = [0.01, 0.02, 0.051, 0.101, 0.2, 0.5, 0.7, 0.8, 1, 2, 4.998]  # мкА
+U4 = [9.99, 19.99, 49.98, 100, 199.9, 499.8, 699.6, 799.6, 999, 2000, 4998]  # мкВ
+
+plt.plot(U4, I4, label='Reverse Germanium', c='r')
+plt.grid()
+plt.legend()
+plt.show()
