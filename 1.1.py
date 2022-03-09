7017e926d550ea493cb6bdd8669d34b5ee45cfcc
0ldStar <brosto_man@mail.ru>
2022-03-10 00:47:33 +0700

    Init

---
+++ 1.1.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def derivative1(x):  # AO
+    return 8 * x / 27 - 6274 / 27
+
+
+def derivative2(x):  # CK
+    return 1 * x / 22 - 721 / 22
+
+
+def derivative3(x):  # OE
+    return 10 * x / 23 - 8006 / 23
+
+
+I1 = [0.0003, 0.000566, 0.5344, 2.452, 4.417, 6.394, 8.376, 10.36, 12.35, 14.34, 16.33, 18.32]
+U1 = [299.9, 499.7, 732.8, 774.2, 791.4, 802.9, 811.9, 819.4, 825.9, 831.7, 837.1, 842]
+
+plt.plot(U1, I1, label='Straight Silicon', c='r')
+
+X1 = np.arange(782, 818)
+plt.plot(X1, derivative1(X1), 'k--')
+X2 = np.arange(720, 743)
+plt.plot(X2, derivative2(X2), 'k--')
+X3 = np.arange(800, 842)
+plt.plot(X3, derivative3(X3), 'k--')
+
+plt.hlines(10, 0, 818, linestyles='--', colors='black')
+plt.vlines(818, 0, 10, linestyles='--', colors='black')
+plt.hlines(1, 0, 743, linestyles='--', colors='black')
+plt.vlines(743, 0, 1, linestyles='--', colors='black')
+
+plt.scatter(818, 10, label='O')
+plt.scatter(818, 0, label='B')
+plt.scatter(800, 0, label='E')
+plt.scatter(782, 0, label='A')
+plt.scatter(743, 1, label='K')
+plt.scatter(743, 0, label='D')
+plt.scatter(720, 0, label='C')
+
+plt.ylabel('I, мА')
+plt.xlabel('U, мВ')
+
+plt.grid()
+plt.legend()
+plt.show()
+
+print("R0 = %d Ом" % (818 / 10))
+print("r0 = %d Ом" % ((818 - 782) / 10))
+print("Rк = %d Ом" % (743 / 1))
+print("rк = %d Ом" % ((743 - 720) / 1))
+print("E = %d мВ" % 800)
+
---
+++ 2.1.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def derivative1(x):  # AO
+    return 0.25 * x - 98.5
+
+
+def derivative2(x):  # CK
+    return 1 * x / 27 - 338 / 27
+
+
+def derivative3(x):  # OE
+    return 10 * x / 29 - 4069 / 29
+
+
+I3 = [0.03731, 0.3245, 1.253, 3.2, 5.172, 7.151, 9.134, 11.12, 13.11, 15.1, 17.09, 19.08]
+U3 = [281.3, 337.7, 373.6, 399.8, 414.2, 424.6, 432.9, 440, 446.2, 451.9, 457, 461.9]
+plt.plot(U3, I3, label='Straight Germanium', c='r')
+
+X1 = np.arange(392, 434)
+plt.plot(X1, derivative1(X1), 'k--')
+X2 = np.arange(336, 365)
+plt.plot(X2, derivative2(X2), 'k--')
+X3 = np.arange(406, 461)
+plt.plot(X3, derivative3(X3), 'k--')
+
+plt.hlines(10, 0, 434, linestyles='--', colors='black')
+plt.vlines(434, 0, 10, linestyles='--', colors='black')
+plt.hlines(1, 0, 365, linestyles='--', colors='black')
+plt.vlines(365, 0, 1, linestyles='--', colors='black')
+
+plt.scatter(434, 10, label='O')
+plt.scatter(434, 0, label='B')
+plt.scatter(392, 0, label='A')
+plt.scatter(406, 0, label='E')
+plt.scatter(365, 1, label='K')
+plt.scatter(365, 0, label='D')
+plt.scatter(336, 0, label='C')
+
+plt.ylabel('I, мА')
+plt.xlabel('U, мВ')
+
+plt.grid()
+plt.legend()
+plt.show()
+
+print("R0 = %d Ом" % (434 / 10))
+print("r0 = %d Ом" % ((434 - 392) / 10))
+print("Rк = %d Ом" % (365 / 1))
+print("rк = %d Ом" % ((365 - 336) / 1))
+print("E = %d мВ" % 406)
